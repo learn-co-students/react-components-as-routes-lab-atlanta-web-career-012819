237daa86940963b457067759eefdadf36798c9bc
github username <github email address>
2019-04-09 10:54:04 -0400

    isntalled and boiler plate coded

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -11,11 +8,30 @@ import Movies from '../components/Movies';
 
 
 const App = (props) => {
-  return (
-    <Router>
-      {/*{code here}*/}
-    </Router>
-  );
+
+    let navLinks = () => {
+        <div>
+            <NavLink to="/" exact style={link} activeStyle={{background: 'darkblue'}}>Home</NavLink>
+            <NavLink to="/actors" exact style={link} activeStyle={{background: 'darkblue'}}>Actors</NavLink>
+            <NavLink to="/directors" exact style={link} activeStyle={{ background: 'darkblue' }}>Directors</NavLink>
+            <NavLink to="/movies" exact style={link} activeStyle={{ background: 'darkblue' }}>Movies</NavLink>
+        </div>;
+    }
+    /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+    /* add styling to Navlink */
+    /* add prop for activeStyle */
+
+    return (
+        <Router>
+            <React.Fragment>
+                <NavLinks />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/actors" component={Actors} />
+                <Route exact path="/directors" component={Directors} />
+                <Route exact path="/movies" component={Movies} />
+            </React.Fragment>
+        </Router>
+    );
 };
 
 export default App
